50be69dc1272ec4ba75107d0ec65a5fe7a528a3e
Adame <242847269+VicenteAdame@users.noreply.github.com>
2026-03-07 08:43:54 -0600

    corregi listado y registro en Registro TAH

--- src/app/components/alumno/alumno.ts
+++ src/app/components/alumno/alumno.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-alumno',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './alumno.html',
   styleUrl: './alumno.css',
 })

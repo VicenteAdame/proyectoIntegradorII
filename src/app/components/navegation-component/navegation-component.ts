fe45f7ec1064a85d51f88fecf83e87c921d94989
Adame <242847269+VicenteAdame@users.noreply.github.com>
2026-03-15 10:16:24 -0600

    Barra de navegacion completada

--- src/app/components/navegation-component/navegation-component.ts
+++ src/app/components/navegation-component/navegation-component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navegation-component',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navegation-component.html',
   styleUrl: './navegation-component.css',
 })
 export class NavegationComponent {
+  @Input() rol: 'admin' | 'empleado' = 'admin';
   usuario$;
 
   constructor(private readonly authService: AuthService) {
--- src/app/layout/empleado-dashboard.component.ts
+++ src/app/layout/empleado-dashboard.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router, RouterOutlet } from '@angular/router';
 import { AuthService } from '../core/services/auth.service';
 import { NavegationComponent } from '../components/navegation-component/navegation-component';
 
 @Component({
   selector: 'app-empleado-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NavegationComponent, RouterOutlet],
   templateUrl: './empleado-dashboard.component.html',
   styleUrl: './empleado-dashboard.component.css'
 })
 export class EmpleadoDashboardComponent {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService, public router: Router) { }
 
   onLogout(): void {
     this.authService.logout();
